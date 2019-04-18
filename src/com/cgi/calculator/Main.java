10f0f5b8bbc603b7b65022dfbac883134b386da5
jbailey5421 <jordanbailey201@gmail.com>
2019-04-18 13:59:01 +0100

    added accessors and mutator

--- src/com/cgi/calculator/Main.java
+++ src/com/cgi/calculator/Main.java
@@ -3,29 +3,26 @@ package com.cgi.calculator;
 public class Main {
 
     public static void main(String[] args) {
-
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};//ARRAY
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};//ARRAY
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
-
       MathsEquation[] equations = new MathsEquation[4];
-      equations[0] = create(100.0d, 50,'d');
-      equations[0] = new MathsEquation();
+      equations[0] = create(100.0d, 50.0d,'d');
+      equations[1] = create(25.0d, 92.0d,'a');
+      equations[2] = create(225.0d, 17.0d,'s');
+      equations[3] = create(11.0d, 3.0d,'m');
+
+
      for (MathsEquation equation : equations) {
          equation.execute();
-
          System.out.print("results = ");
-         System.out.println(equation.result);
+         System.out.println(equation.getResult());
 
      }
 
     }
     public static MathsEquation create(double leftVal, double rightVal, char opCode) {
         MathsEquation equation = new MathsEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpCode(opCode);
 
         return equation;
     }
