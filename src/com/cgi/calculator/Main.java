a29bb3f083166fdf4129c82021b6d117ba5f8435
jbailey5421 <jordanbailey201@gmail.com>
2019-04-25 09:27:49 +0100

    added exception handling for non numeric data, incorrect number of fields and invalid commands
    Signed-off-by: jbailey5421 <jordanbailey201@gmail.com>

--- src/com/cgi/calculator/Main.java
+++ src/com/cgi/calculator/Main.java
@@ -7,16 +7,27 @@ public class Main {
     public static void main(String[] args) {
 
     String[] statements = {
+      "add 1.0",
+      "add xx 25.0",
+      "addX 0.0 0.0",
       "divide 100.0 50.0",
       "add 25.0 92.0",
       "subtract 225.0 17.0",
       "multiply 11.0 3.0"
     };
     CalculateHelper helper = new CalculateHelper();
-            for(String statement:statements) {
-                helper.process(statement);
-                System.out.println(helper);
-            }
+
+    for(String statement:statements) {
+        try {
+            helper.process(statement);
+            System.out.println(helper);
+        }catch (InvalidStatementException e) {
+            System.out.println(e.getMessage());
+            if(e.getCause()!= null)
+                System.out.println("original exception: " + e.getCause().getMessage());
+        }
+    }
+
         MathsEquation[] equations = new MathsEquation[4];
         equations[0] = new MathsEquation('d', 100.0d, 50.0d);
         equations[1] = new MathsEquation('a', 25.0d, 92.0d);
